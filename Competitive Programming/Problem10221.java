5dbf1dbcc41dde522a59c08286d984f9cda051c2
KhaoulaIB <115530714+KhaoulaIB@users.noreply.github.com>
2023-11-01 12:11:10 +0100

    Satellites

---
+++ Competitive Programming/Problem10221.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    private static int radius = 6440;
+
+    public static void main(String[] args) {
+        // Establecer la localización a una que use el punto como separador decimal
+        Locale.setDefault(Locale.US);
+
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            double s = sc.nextDouble();
+            double grado = sc.nextDouble();
+            String unidad = sc.next();
+
+            if (unidad.equals("deg")) {
+                grado = Math.toRadians(grado);
+            } else if (unidad.equals("min")) {
+                grado = Math.toRadians(grado / 60);
+            }
+
+            double arco =  (s + radius) * grado;
+            double chore = 2 * (s + radius) * Math.sin(grado / 2);
+
+            System.out.printf("%.6f %.6f%n", arco, chore);
+        }
+    }
+}
