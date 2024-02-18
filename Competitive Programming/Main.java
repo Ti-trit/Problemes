2064a705d454f9224d946a7481abbc75de6d9956
KhaoulaIB <115530714+KhaoulaIB@users.noreply.github.com>
2024-02-18 20:47:39 +0100

    913 Joana and the Odd Numbers

--- Competitive Programming/Main.java
+++ Competitive Programming/Main.java
@@ -5,16 +5,15 @@ public class Main {
 
 
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()) {
+            while (sc.hasNext()) {
 
-            long n = sc.nextLong(); // Se utiliza long en lugar de int para manejar números grandes
-           long d = (int) n/2;
-           long number = d*d;    //cuantos numeros impares hay
-            long start = (number*2) +1;  //start of line of index n
-            //start of thirdlastelement
-            long index = start + 2*(n-3);
-            long sum = index*3 + 6;
-            System.out.println(sum);
+                long n = sc.nextLong(); // Se utiliza long en lugar de int para manejar números grandes
+                long d = (int) n/2;
+                long start = (d*d*2) +1;  //start of line of index n
+                //start of thirdlastelement
+                long index = start + 2*(n-3);
+                long sum = index*3 + 6;
+                System.out.println(sum);
 
         }
     }
